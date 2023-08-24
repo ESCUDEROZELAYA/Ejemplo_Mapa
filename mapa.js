d813aa97e54ebe83afa52ccf0b3239e2aafe36d3
ESCUDEROZELAYA <143060613+ESCUDEROZELAYA@users.noreply.github.com>
2023-08-24 00:31:57 -0500

    Add files via upload

---
+++ mapa.js
@@ -0,0 +1,12 @@
+let map = L.map('map').setView([-12.187626,-76.944735],12)
+
+//Mapa base
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+document.getElementById('select-location').addEventListener('change',function(e){
+  let coords = e.target.value.split(",");
+  map.flyTo(coords,18.5);//Lista desplegable
+})
+
